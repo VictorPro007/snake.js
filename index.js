3e9d411a7fdbd57d80be0a41208af24b6c64d32d
Victor007 <72664387+VictorPro007@users.noreply.github.com>
2020-12-28 20:33:58 +0100

    Add files via upload

---
+++ index.js
@@ -0,0 +1,29 @@
+const os = require('os');
+const figlet = require('figlet');
+const chalk = require('chalk');
+var osType = '';
+switch (os.type()) {
+    case 'Linux': 
+        osType = 'Linux';
+    break;
+    case 'Darwin':
+        osType = 'MacOS';
+    break;
+    case 'Windows_NT':
+        osType = 'Windows';
+    break;
+}
+console.log(chalk.greenBright.bold(figlet.textSync(osType, 
+{font: 'Red Phoenix',
+horizontalLayout: 'default',
+verticalLayout: 'default',
+width: 200,
+whitespaceBreak: true}, function (err, data) {
+    if (err) {
+        console.log('Something went wrong...');
+        console.dir(err);
+        return;
+    }
+    console.log(data);
+})));
+console.log(chalk.yellow.bold(`\n\n\n\nKernel/Core: ${ os.type() }\n\nOS version: ${ os.version() }\n\nCPU Arch: ${ os.arch()}\n\nPlatform: ${ os.platform() }\n\nHomedir: ${ os.homedir() }\n\nTotal memory: ${ os.totalmem } bytes\n\nHostname: ${ os.hostname }\n`));
